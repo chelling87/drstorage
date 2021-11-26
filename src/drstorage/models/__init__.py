3caafca3d845df91e868e5ca8412b101210ce81d
Cole Helling <chelling@lxplus.cern.ch>
2021-11-26 08:29:42 -0800

    added our Dr Storage model

--- src/drstorage/models/__init__.py
+++ src/drstorage/models/__init__.py
@@ -1,5 +1,6 @@
 from .base import generic
 from .f1 import F1_600, F1_1200
 from .x2m import X2M_157
+from .x2b import X2B_400
 
-__all__ = ["generic", "F1_600", "F1_1200", "X2M_157"]
+__all__ = ["generic", "F1_600", "F1_1200", "X2M_157", "X2B_400"]
---
+++ src/drstorage/models/x2b.py
@@ -0,0 +1,3 @@
+from .base import DrStorageFactory
+
+X2B_400 = DrStorageFactory(model_number=b"\x00\xfa")
